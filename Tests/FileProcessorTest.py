9b9aa7c78e569ec05a086f0044679e4ffee33d71
Corrado Calzoni <corrado.calzoni@roche.com>
2019-04-08 23:49:02 +0200

    Problem when path does not exist

--- Tests/FileProcessorTest.py
+++ Tests/FileProcessorTest.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 
 class FileProcessor:
     '''A File Processor class to detect files and folders.'''
@@ -9,18 +10,31 @@ class FileProcessor:
 
     def Run(self):
         self.fileListReport = self.path
+        self.ListFiles()
 
     def SimulateListFiles(self):
         self.fileListReport += "\n"
-        self.fileListReport += "Text1.txt"
+        self.fileListReport += "/SD/Folder1/Text1.txt"
+        
+    def ListFiles(self):
+        # r=root, d=directories, f = files
+        for r, d, f in os.walk(self.path):
+            for file in f:
+                self.fileListReport += '\n'
+                self.fileListReport += os.path.join(r, file)
 
 class FileProcessorTest(unittest.TestCase):
 
-    def test_FileProcessorListFile_Run(self):
+    def test_FileProcessorListFile_Constructor(self):
         fileLister = FileProcessor("/SD/Folder1")
         self.assertEqual(fileLister.fileListReport, "")
 
-    def test_FileProcessorListFile_Constructor(self):
+    def test_FileProcessorListFile_PathNotExist(self):
+        fileLister = FileProcessor("/SD/DoesNotExist")
+        fileLister.Run()
+        self.assertEqual(fileLister.fileListReport, "[Error: Path does not exist] /SD/DoesNotExist", "Incorrect Report generated.")
+
+    def test_FileProcessorListFile_Run(self):
         fileLister = FileProcessor("/SD/Folder1")
         fileLister.Run()
         self.assertEqual(fileLister.fileListReport, "/SD/Folder1")
@@ -30,7 +44,7 @@ class FileProcessorTest(unittest.TestCase):
         fileLister.Run()
         fileLister.SimulateListFiles()
         
-        expectedReport = ['/SD/Folder1','Text1.txt']
+        expectedReport = ['/SD/Folder1','/SD/Folder1/Text1.txt']
         actualReport = fileLister.fileListReport.splitlines()
         self.assertListEqual(expectedReport, actualReport, "fileListReport does not contain the expected values.")
 
