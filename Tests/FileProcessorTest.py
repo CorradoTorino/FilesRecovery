9e478b2e57c32f0c80557da7bf1be7b56be7a0ce
Corrado Calzoni <corrado.calzoni@roche.com>
2019-04-23 23:00:22 +0200

    Split production code from test code

--- Tests/FileProcessorTest.py
+++ Tests/FileProcessorTest.py
@@ -1,41 +1,7 @@
 import unittest
 import os
-
-class FileProcessor:
-    '''A File Processor class to detect files and folders.'''
-
-    def __init__(self, path):
-        start = "\\"
-        if(path.startswith(start)):
-            self.path = os.getcwd() + path
-        else:
-            self.path = path
-        
-        self.fileListReport = ''
-
-    def Run(self):
-        if(self.CheckPath()):
-            self.__ListFiles()
-    
-    def __ListFiles(self):
-        # r=root, d=directories, f = files
-        for r, d, f in os.walk(self.path):
-            for directory in d:
-                self.fileListReport += '\n'
-                self.fileListReport += os.path.join(r, directory)
-
-            for file in f:
-                self.fileListReport += '\n'
-                self.fileListReport += os.path.join(r, file)
-
-    def CheckPath(self):
-        if(os.path.isdir(self.path)):
-            self.fileListReport = self.path
-            return True
-        else:
-            self.fileListReport += "[Error: Path does not exist] "
-            self.fileListReport += self.path
-            return False
+import sys
+from FileProcessor import FileProcessor
 
 class FileProcessorTest(unittest.TestCase):
 
