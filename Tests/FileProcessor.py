a8701f618e9ee486aacba64ea9ac01742a60783a
Corrado Calzoni <corrado.calzoni@roche.com>
2019-04-26 22:44:34 +0200

    started copy feature

--- Tests/FileProcessor.py
+++ Tests/FileProcessor.py
@@ -3,14 +3,25 @@ import os
 class FileProcessor:
     '''A File Processor class to detect files and folders.'''
 
-    def __init__(self, path):
-        start = "\\"
-        if(path.startswith(start)):
-            self.path = os.getcwd() + path
-        else:
-            self.path = path
-        
+    def __init__(self, inputPath, destinationPath = None):
+        self.__setInputPath(inputPath) 
+        self.__setDestinationPath(destinationPath)
+
         self.fileListReport = ''
+
+    def __setInputPath(self, inputPath):
+        escapedBackslash = "\\"
+        if(inputPath.startswith(escapedBackslash)):
+            self.inputPath = os.getcwd() + inputPath
+        else:
+            self.inputPath = inputPath
+
+    def __setDestinationPath(self, destinationPath):
+        escapedBackslash = "\\"
+        if destinationPath is None or destinationPath.startswith(escapedBackslash) is False:
+            self.destinationPath = destinationPath
+        else:
+            self.destinationPath = os.getcwd() + destinationPath
 
     def Run(self):
         if(self.CheckPath()):
@@ -18,7 +29,7 @@ class FileProcessor:
     
     def __ListFiles(self):
         # r=root, d=directories, f = files
-        for r, d, f in os.walk(self.path):
+        for r, d, f in os.walk(self.inputPath):
             for directory in d:
                 self.fileListReport += '\n'
                 self.fileListReport += os.path.join(r, directory)
@@ -28,10 +39,17 @@ class FileProcessor:
                 self.fileListReport += os.path.join(r, file)
 
     def CheckPath(self):
-        if(os.path.isdir(self.path)):
-            self.fileListReport = self.path
+        if(os.path.isdir(self.inputPath)):
+            self.fileListReport = self.inputPath
+
+            if self.destinationPath is not None:             
+                # create the directory in the destination folder
+                directoryName = os.path.split(self.inputPath)[1]
+                copiedDirectory = os.path.join(self.destinationPath, directoryName)
+                os.mkdir(copiedDirectory)
+
             return True
         else:
             self.fileListReport += "[Error: Path does not exist] "
-            self.fileListReport += self.path
+            self.fileListReport += self.inputPath
             return False
---
+++ Tests/FileProcessorCopyFilesTest.py
@@ -0,0 +1,65 @@
+import unittest
+import os
+import sys
+import shutil
+
+from FileProcessor import FileProcessor
+
+class FileProcessorCopyFilesTest(unittest.TestCase):
+
+    def test_FileProcessorCopyFilesTest_Run_EmptyFolder(self):
+
+        inputFolder = r"\Tests\SD\EmptyFolder"
+        outputFolder = r"\Tests\TestOuput"
+
+        expectedReport = [
+            os.getcwd() + r'\Tests\SD\EmptyFolder',]
+        expectedReportForDestination = [
+            os.getcwd() + r'\Tests\TestOuput',
+                os.getcwd() + r'\Tests\TestOuput\EmptyFolder',]
+        
+        self.__testFileProcessorCopying(inputFolder, outputFolder, expectedReport, expectedReportForDestination)
+
+    def test_FileProcessorCopyFilesTest_Run_FolderWithFile(self):
+        
+        inputFolder = r"\Tests\SD\FolderWithFile"
+        outputFolder = r"\Tests\TestOuput"
+        
+        expectedReport = [
+            os.getcwd() + r'\Tests\SD\FolderWithFile',
+            os.getcwd() + r'\Tests\SD\FolderWithFile\Text1.txt']
+        
+        self.__testFileProcessorCopying(inputFolder, outputFolder, expectedReport)
+
+    def __cleanTestOuput(self):     
+        testOuputDirectory = os.getcwd() + r"\Tests\TestOuput" 
+        try:
+            shutil.rmtree(testOuputDirectory)
+        except FileNotFoundError:
+            pass
+        os.mkdir(testOuputDirectory)
+
+    def __testFileProcessorCopying(self, inputFolder, outputFolder, expectedReport, expectedReportForDestination):
+            self.__cleanTestOuput()
+
+            fileCopier = FileProcessor(inputFolder, outputFolder)
+            fileCopier.Run()
+            print(fileCopier.fileListReport)
+            
+            actualReport = fileCopier.fileListReport.splitlines()
+            self.assertEqual(len(expectedReport), len(actualReport), "fileListReport does not contain the expected number of entries.")
+            self.assertListEqual(expectedReport, actualReport, "fileListReport does not contain the expected values.")   
+
+            self.__checkDirectoryTreeCopied(outputFolder, expectedReportForDestination)
+
+    def __checkDirectoryTreeCopied(self, destinationFolder, expectedReport):
+        fileLister = FileProcessor(destinationFolder)
+        fileLister.Run()
+        print(fileLister.fileListReport)
+        
+        actualReport = fileLister.fileListReport.splitlines()
+        self.assertEqual(len(expectedReport), len(actualReport), "fileListReport does not contain the expected number of entries.")
+        self.assertListEqual(expectedReport, actualReport, "fileListReport does not contain the expected values.")   
+
+if __name__ == '__main__':
+    unittest.main()
